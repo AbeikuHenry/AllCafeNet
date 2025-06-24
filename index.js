0fe42d4afe0fe8e001f939d24e828bbeaa00b68a
lamptey07 <lamptey07@gmail.com>
2025-06-24 21:12:10 +0200

    fix: await login status in Mikrotik API connection

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.post("/verify-payment", async (req, res) => {
   try {
     const conn = await api.connect();
     console.log("Connected to Mikrotik successfully");
-    console.log("Status: ", conn.login)
+    console.log("Status: ", await conn.login())
 
     const response = await api.write([
       "/user/print",
